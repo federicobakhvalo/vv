bbf7728ec355c47c5b1158ba0f361ade670acbb9
federicobakhvalo <bakhvalov.fedya@yandex.ru>
2025-03-18 08:45:44 +0200

    first_f

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+import requests
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Test:
+    def get(self):
+        return 'hello'
+
+
+ex = Test()
+print(ex.get())
